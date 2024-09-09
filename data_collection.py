99cf250c2a26887790f40d7935f7829df89aa8f5
r8ndeer <liboting.lance2227@gmail.com>
2024-09-09 16:10:12 +0100

    feat: subscribe to the Image messages by ROS and show it using opencv

--- data_collection.py
+++ data_collection.py
@@ -2,6 +2,12 @@
 import rospy
 from sensor_msgs.msg import Image
 
+import numpy as np
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
+import time
+
 class WristCameraListener:
 
     def __init__(self) -> None:
@@ -11,10 +17,35 @@ class WristCameraListener:
             self.callback,
             queue_size=1
             )
+        self.bridge = CvBridge()
+
+        # To get the publish rate
+        self.last_time = None
+        self.time_diff = []
 
 
-    def callback(self, data):
-        rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    def callback(self, image_data):
+        rospy.loginfo(f"Received an image of size {image_data.width}x{image_data.height}")
+
+        try:
+            self.image = self.bridge.imgmsg_to_cv2(image_data)
+
+            cv2.imshow("Left Arm Camera Image", self.image)
+            cv2.waitKey(1)  # Display image for 1ms and process any OpenCV window events
+
+            # get the rate of message
+            if self.last_time is not None:
+                current_time = time.time()
+                self.time_diff.append(current_time - self.last_time)
+                self.last_time = current_time
+            else:
+                self.last_time = time.time()
+            rate = 1 / np.average(self.time_diff)
+            rospy.loginfo(f"Message rate: {rate} Hz")
+
+        except CvBridgeError as e:
+            rospy.loginfo(f"Error converting ROS Image to OpenCV: {e}")
+
 
 
 def main():
